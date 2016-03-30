566a7061b4b5802fd8dc36e61e6b9ba0c825788b
jianjian.mei <mjjde@qq.com>
2016-03-30 11:45:20 +0800

    fix location

--- app/src/main/java/com/open_source/joker/concentration/activity/LocationActivity.java
+++ app/src/main/java/com/open_source/joker/concentration/activity/LocationActivity.java
@@ -1,9 +1,14 @@
 package com.open_source.joker.concentration.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.open_source.joker.concentration.R;
 import com.open_source.joker.concentration.app.CONActivity;
+import com.open_source.joker.concentration.location.geo.GeoListener;
+import com.open_source.joker.concentration.location.geo.GeoServiceImpl;
+
+import java.util.Map;
 
 /**
  * 文件名：com.open_source.joker.concentration.activity
@@ -11,10 +16,25 @@ import com.open_source.joker.concentration.app.CONActivity;
  * 时间：16/3/17
  * 作者: joker
  */
-public class LocationActivity extends CONActivity {
+public class LocationActivity extends CONActivity implements GeoListener {
+    GeoServiceImpl loc;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        tv = (TextView)findViewById(R.id.result);
+        loc = GeoServiceImpl.getInstance(this);
+        loc.addListener(this);
+        loc.requestGeoParams();
+    }
+
+    @Override
+    public void onRequestGeoParamsFinish(Map<String, String> params) {
+        StringBuffer sb = new StringBuffer();
+        for (String key : params.keySet()) {
+            sb.append("key:"+key+"  value:"+params.get(key)+"\n");
+        }
+        tv.setText(sb.toString());
     }
 }
